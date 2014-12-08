6d357d5638c2e43ea2b46d81096009307a255009
ZapriyRoman <zapriy.05@mail.ru>
2014-12-09 00:02:46 +0200

    контекстное меню, в нём работает открыть и инфо

--- src/com/itstep/commander/AppMainActivity.java
+++ src/com/itstep/commander/AppMainActivity.java
@@ -2,6 +2,7 @@ package com.itstep.commander;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 
 import android.app.AlertDialog;
 import android.app.ListActivity;
@@ -11,13 +12,23 @@ import android.content.Intent;
 import android.content.DialogInterface.OnClickListener;
 import android.net.Uri;
 import android.os.Bundle;
-import android.view.View;
+import android.view.*;
 import android.webkit.MimeTypeMap;
 import android.widget.*;
 
 public class AppMainActivity extends ListActivity {
+
     private ArrayList<String> directoryEntries = new ArrayList<String>();
     private File currentDirectory = new File("/");
+    private static final int IDM_OPEN = 101;
+    private static final int IDM_COPY = 102;
+    private static final int IDM_CUT = 103;
+    private static final int IDM_PASTE = 104;
+    private static final int IDM_DELETE = 105;
+    private static final int IDM_RENAME = 106;
+    private static final int IDM_INFO = 107;
+    private int position;
+
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -25,18 +36,7 @@ public class AppMainActivity extends ListActivity {
        browseTo(new File("/"));
 
        ListView lv = getListView();
-        lv.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
-
-            public boolean onItemLongClick(AdapterView<?> arg0, View arg1,
-                                           int pos, long id) {
-
-                Toast toast = Toast.makeText(getApplicationContext(),
-                        "контекстное меню!", Toast.LENGTH_SHORT);
-                toast.show();
-
-                return true;
-            }
-        });
+        registerForContextMenu(lv);
     }
 
     private void upOneLevel(){
@@ -78,19 +78,17 @@ public class AppMainActivity extends ListActivity {
                 }
             };
 
-            //create dialog
             new AlertDialog.Builder(this)
-                    .setTitle("Подтверждение") //title
-                    .setMessage("Хотите открыть файл "+ aDirectory.getName() + "?") //message
-                    .setPositiveButton("Да", okButtonListener) //positive button
-                    .setNegativeButton("Нет", cancelButtonListener) //negative button
-                    .show(); //show dialog
+                    .setTitle("Подтверждение")
+                    .setMessage("Хотите открыть файл " + aDirectory.getName() + "?")
+                    .setPositiveButton("Да", okButtonListener)
+                    .setNegativeButton("Нет", cancelButtonListener)
+                    .show();
         }
     }
 
     private void fill(File[] files) {
         ArrayList<String> names = new ArrayList<String>();
-        //clear list
         directoryEntries.clear();
         names.clear();
 
@@ -99,34 +97,46 @@ public class AppMainActivity extends ListActivity {
             names.add("..");
         }
 
-        //add every file into list
         for (File file : files) {
             directoryEntries.add(file.getAbsolutePath());
             names.add(file.getName());
         }
 
-        //create array adapter to show everything
         ArrayAdapter<String> directoryList = new ArrayAdapter<String>(this, R.layout.row, names);
         setListAdapter(directoryList);
     }
 
-    //when you clicked onto item
     @Override
     protected void onListItemClick(ListView l, View v, int position, long id) {
-        //get selected file name
+
         int selectionRowID = position;
         String selectedFileString = directoryEntries.get(selectionRowID);
 
-        //if we select ".." then go upper
         if(selectedFileString.equals("..")){
             upOneLevel();
         } else {
-            //browse to clicked file or directory using browseTo()
+
             File clickedFile = null;
             clickedFile = new File(selectedFileString);
             if (clickedFile != null)
                  browseTo(clickedFile);
         }
+    }
+    public long getDirectoryLength(File f){
+        long sum = 0;
+        if(f.isFile()) {
+            return f.length();
+        }else{
+            File[] arrPath = f.listFiles();
+            if(arrPath.length > 0) {
+                for (File file : arrPath) {
+                    sum += getDirectoryLength(file);
+                }
+            }else{
+                return sum;
+            }
+        }
+        return  sum;
     }
     public String get_mime_by_filename(String filename){
         String ext;
@@ -142,5 +152,109 @@ public class AppMainActivity extends ListActivity {
             }
         }
         return "application/octet-stream";
+    }
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo)
+    {
+        super.onCreateContextMenu(menu, v, menuInfo);
+
+        AdapterView.AdapterContextMenuInfo aMenuInfo = (AdapterView.AdapterContextMenuInfo) menuInfo;
+
+        // Получаем позицию элемента в списке
+        position = aMenuInfo.position;
+        menu.add(Menu.NONE, IDM_OPEN, Menu.NONE, "Открыть");
+        menu.add(Menu.NONE, IDM_COPY, Menu.NONE, "Копировать");
+        menu.add(Menu.NONE, IDM_CUT, Menu.NONE, "Вырезать");
+        menu.add(Menu.NONE, IDM_PASTE, Menu.NONE, "Вставить");
+        menu.add(Menu.NONE, IDM_DELETE, Menu.NONE, "Удалить");
+        menu.add(Menu.NONE, IDM_RENAME, Menu.NONE, "Переименовать");
+        menu.add(Menu.NONE, IDM_INFO, Menu.NONE, "Инфо");
+        menu.setHeaderTitle("Меню");
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item)
+    {
+        File f = new File(directoryEntries.get(position));
+        switch (item.getItemId())
+        {
+
+            case IDM_OPEN:
+                browseTo(f);
+                break;
+            case IDM_COPY:
+
+                break;
+            case IDM_CUT:
+
+                break;
+            case IDM_PASTE:
+
+                break;
+            case IDM_DELETE:
+
+                break;
+            case IDM_RENAME:
+               // f.renameTo(File newPath)
+                break;
+            case IDM_INFO:
+
+                if(f.isFile()) {
+                    new AlertDialog.Builder(this)
+                            .setTitle("Информация")
+                            .setMessage("тип: файл" +
+                                            "\nабсолютный путь: \n" +
+                                            f.getAbsolutePath() +
+                                            "\nдоступно для чтения: " +
+                                            (f.canRead() ? "да" : "нет") +
+                                            "\nдоступно для записи: " +
+                                            (f.canWrite() ? "да" : "нет") +
+                                            "\nскрытый: " +
+                                            (f.isHidden() ? "да" : "нет") +
+                                            "\nразмер: " +
+                                            f.length()/1024f/1024f + "MB" +
+                                            "\nдата последней модификации: " +
+                                            new Date(f.lastModified()).toString()
+
+                            )
+                            .setCancelable(false)
+                            .setNegativeButton("ОК",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    })
+                            .show();
+                }else{
+                    new AlertDialog.Builder(this)
+                            .setTitle("Информация")
+                            .setMessage("тип: каталог" +
+                                            "\nабсолютный путь: \n" +
+                                            f.getAbsolutePath() +
+                                            "\nдоступно для чтения: " +
+                                            (f.canRead() ? "да" : "нет") +
+                                            "\nскрытый: " +
+                                            (f.isHidden() ? "да" : "нет") +
+                                            "\nразмер: " +
+                                            getDirectoryLength(f)/1024f/1024f + "MB" +
+                                            "\nдата последней модификации: " +
+                                            new Date(f.lastModified()).toString()
+                            )
+                            .setCancelable(false)
+                            .setNegativeButton("ОК",
+                                    new DialogInterface.OnClickListener() {
+                                        public void onClick(DialogInterface dialog, int id) {
+                                            dialog.cancel();
+                                        }
+                                    })
+                            .show();
+                }
+                break;
+            default:
+                return super.onContextItemSelected(item);
+        }
+        return true;
     }
 }
